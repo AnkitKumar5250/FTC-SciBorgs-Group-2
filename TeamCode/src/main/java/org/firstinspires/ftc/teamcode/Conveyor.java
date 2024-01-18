d44eda69659dd061057822003ed4b54d5725314f
Slothiane <152555537+Slothiane@users.noreply.github.com>
2024-01-18 16:21:37 -0500

    Update Conveyor.java
    Added some comments to the motor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Conveyor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Conveyor.java
@@ -9,9 +9,9 @@ public class Conveyor {
 
     public void Init(HardwareMap hardwareMap) {
         motor = hardwareMap.get(DcMotor.class,"Conveyor Motor");
-        motor.setDirection(DcMotor.Direction.FORWARD);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); 
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setDirection(DcMotor.Direction.FORWARD); // motor will move forward when setPower is positive
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // motor immediately stops when setPower == 0; Does not float/glide
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // the encoder position is set to zero and motor stops; must use another setMode option(RUN_WITHOUT_ENCODER, RUN_TO_POSITION, RUN_WITH_ENCODER) to start motor again
     }
     public void UpdateConveyorPower(Input input) {
         if (input.inputMode != "Conveyor") {
