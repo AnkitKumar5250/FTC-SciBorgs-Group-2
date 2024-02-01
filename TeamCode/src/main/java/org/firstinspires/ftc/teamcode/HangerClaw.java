06317c5d7711493fc62446a2e6ef2840198105d2
Slothiane <152555537+Slothiane@users.noreply.github.com>
2024-02-01 16:36:42 -0500

    Update HangerClaw.java
    Corrected some code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HangerClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HangerClaw.java
@@ -50,13 +50,13 @@ public class Claw {
 
     public void open() {
         // code for opening the claw
-        hand.setposition(shape_limit);
+        hand.setPosition(shape_limit);
     }
     public void retract() {
         // code for retracting the arm
     }
     public void close() {
         // code for closing the claw
-        hand.setposition(0);
+        hand.setPosition(0);
     }
 }
