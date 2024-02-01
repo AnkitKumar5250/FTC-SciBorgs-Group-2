9376a8ec58cf493929ec6e154412f24d9ad42801
Slothiane <152555537+Slothiane@users.noreply.github.com>
2024-02-01 16:21:06 -0500

    Update and rename LinearSlide.java to LinearSlide+Claw.java
    Added some claw code to the Linear Slide, also removed left & right DC motors, replaced with a single slide DcMotor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlide+Claw.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class LinearSlide {
+  public DcMotor slide; // motor to move rigging
+  //public DcMotor slideRight;
+  public Servo claw; // grabbing the game pieces
+  public Servo claw_limit; // rotation limit such that the claw is able to grab the game pieces
+  
+  public void Init(HardwareMap hardwareMap) {
+    // initialize motor on the linear slide
+    slide = hardwareMap.get(DcMotor.class, "Linear Slide DcMotor"); 
+    //slideRight = hardwareMap.get(DcMotor.class, "Linear Slide Right DcMotor");
+    claw = hardwareMap.get(Servo.class, "Linear Slide Claw Servo");
+
+    // sets instructions for linear slide motor
+    slide.setDirection(DcMotor.Direction.FORWARD); // motor will move forward when setPower is positive
+    slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // motor immediately stops when setPower == 0; Does not float/glide
+    slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // the encoder position is set to zero and motor stops; must use another setMode option(RUN_WITHOUT_ENCODER, RUN_TO_POSITION, RUN_WITH_ENCODER) to start motor again
+
+    // sets instructions for right linear slide motor
+    //slideRight.setDirection(DcMotor.Direction.FORWARD); // motor will move forward when setPower is positive
+    //slideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // motor immediately stops when setPower == 0; Does not float/glide
+    //slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // the encoder position is set to zero and motor stops; must use another setMode option(RUN_WITHOUT_ENCODER, RUN_TO_POSITION, RUN_WITH_ENCODER) to start motor again
+    }
+
+    public void Update(Input input) {
+      if (input.inputMode != "claw") {
+              return;
+          }
+          if (input.current.x != input.previous.x) {
+              if (input.current.x) {
+                  open();
+                  return;
+              }
+              close();
+          }
+          if (input.current.y != input.previous.y) {
+              if (input.current.x) {
+                  close();
+                  return;
+              }
+              open();
+      }
+    public void open() {
+        // code for opening the claw
+        claw.setposition(claw_limit);
+    }
+    public void retract() {
+        // code for retracting the slide
+    }
+    public void close() {
+        // code for closing the claw
+        claw.setposition(0);
+    }
+}
